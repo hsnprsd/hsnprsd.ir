4a635a4f7a51a4b3a338cbdcbaf2f4c0cd23ea82
Ehsan Poursaeed <hsnprsd@gmail.com>
2024-05-27 23:35:58 +0330

    hello, world

---
+++ content/hello-world.rst
@@ -0,0 +1,11 @@
+Hello, World!
+#############
+
+:date: 2024-05-27 19:56
+:modified: 2024-05-27 19:56
+:tags: hello, world
+:category: Dev
+:slug: hello-world
+:authors: Ehsan Poursaeed
+
+Hello, World!
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'Ehsan Poursaeed'
+SITENAME = 'hsnprsd'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Tehran'
+
+DEFAULT_LANG = 'fa'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+)
+
+# Social widget
+SOCIAL = (
+    ('GitHub', 'https://github.com/hsnprsd'),
+    ('Telegram', 'https://telegram.me/hsnprsd'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
